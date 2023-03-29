25f45841d0b8e29ea154de5661e38481c3232dfb
Oleksii Rozhnenko <qwimik@gmail.com>
2023-03-30 01:04:12 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './js/home-header';
+import './js/library-header';
+import './js/main';
+import './js/pagination';
+import './js/footer';
+
+import './js/modal-details';
+import './js/modal-team';
---
+++ src/js/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = '3217ae06898d6f0a1d4d89e84bb85582';
+
+axios.defaults.baseURL = BASE_URL;
+
+export const searchTrending = async page => {
+  const res = await axios.get(
+    `/trending/all/day?api_key=${API_KEY}&page=${page}`
+  );
+  return res.data;
+};
+
+export const searchMovieId = async id => {
+  const res = await axios.get(`/movie/${id}?api_key=${API_KEY}&language=en-US`);
+  return res.data;
+};
+
+export const searchMovieTitle = async (title, page) => {
+  const res = await axios.get(
+    `/search/movie?api_key=${API_KEY}&page=${page}&language=en-US&include_adult=false&query=${title}`
+  );
+  return res.data;
+};
