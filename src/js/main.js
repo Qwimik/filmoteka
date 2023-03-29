45286453d7bc638de4678f4602f39b83b499397d
Oleksii Rozhnenko <qwimik@gmail.com>
2023-03-30 01:08:01 +0300

    add trycatch to main.js comment

--- src/js/main.js
+++ src/js/main.js
@@ -4,22 +4,34 @@
 // // приклад запросу через api
 // import * as API from './api';
 
-// API.searchTrending(3).then(res => {
-//   // 3 - це сторінка запиту (page)
-//   // тут рисуємо інтерфейс
-//   console.log(res);
-// });
+// try {
+//   API.searchTrending(3).then(res => {
+//     // 3 - це сторінка запиту (page)
+//     // тут рисуємо інтерфейс
+//     console.log(res);
+//   });
+// } catch (error) {
+//   console.log(error);
+// }
 
-// API.searchMovieId(76600).then(res => {
-//   // 76600 - це id фільму для модалки
-//   // тут рисуємо інтерфейс
-//   console.log(res);
-// });
+// try {
+//   API.searchMovieId(76600).then(res => {
+//     // 76600 - це id фільму для модалки
+//     // тут рисуємо інтерфейс
+//     console.log(res);
+//   });
+// } catch (error) {
+//   console.log(error);
+// }
 
-// API.searchMovieTitle('Avatar', 2).then(res => {
-//   // 'Avatar' - це з інпуту назва фільму, який шукаємо
-//   // 2 - це сторінка запиту (page)
-//   // тут рисуємо інтерфейс
-//   console.log(res);
-// });
+// try {
+//   API.searchMovieTitle('Avatar', 2).then(res => {
+//     // 'Avatar' - це з інпуту назва фільму, який шукаємо
+//     // 2 - це сторінка запиту (page)
+//     // тут рисуємо інтерфейс
+//     console.log(res);
+//   });
+// } catch (error) {
+//   console.log(error);
+// }
 //=======================================================
