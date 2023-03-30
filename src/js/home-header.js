2ed360947dd0427937aadb06fc537993dba29b3f
INovak90 <112341296+INovak90@users.noreply.github.com>
2023-03-30 16:06:10 +0300

    Added some JS
    Нужен шаблон карточки и хедер с формой, для рендера разметки и правки ошибок в случаи их появления. Пока всё закомментировал. Ссылки на дом элементы поставлял для пробы, поменяю.

--- src/js/home-header.js
+++ src/js/home-header.js
@@ -1,1 +1,24 @@
 // home-header.js
+// import { searchMovieTitle } from './api';
+
+// const formEl = document.querySelector('form');
+// formEl.addEventListener('submit', onSubmitForm);
+// async function onSubmitForm(e) {
+//   e.preventDefault();
+//   const text = e.target.elements[0].value;
+//   if (!text) {
+//     return console.log('Заполните поле');
+//   }
+
+//   const response = await searchMovieTitle(text, 1);
+//   const { results, total_pages, total_results } = response;
+//   if (results.length === 0) {
+//     return console.log('Введите что-то нормальное');
+//   }
+// }
+
+// function renderMarkup(array) {
+//   array.map(el => {
+//     console.log(el);
+//   });
+// }
