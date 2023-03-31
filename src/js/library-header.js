04f0bf85f9b0381bfbcf79bb9aa689be7326eabc
GioMaglakelidze <giorgio1335@gmail.com>
2023-03-31 23:55:28 +0300

    Update library-header.js

--- src/js/library-header.js
+++ src/js/library-header.js
@@ -1,1 +1,25 @@
-// library-header.js
+// імпортуємо функції
+import { addFilmToLs } from './storage-service';
+import { removeFilmFromLs } from './storage-service';
+import { getFilms } from './storage-service';
+
+// створимо функцію handleClick, яку будемо викликати під час кліку на кнопку:
+function handleClick(event) {
+  const key = event.target.dataset.action; // отримуємо значення data-action кнопки
+
+  if (!key) return; // якщо значення не задано, нічого не робимо
+
+  const films = getFilms(key); // отримуємо масив фильмів з Local Storage по ключу
+
+  // викликаємо функцію рендерінга розмітки, передаючи в неї масив фильмів
+  renderMarkup(films);
+}
+
+// Знаходимо кнопки за атрибутами
+const buttonWatched = document.querySelector('button[data-action]');
+const buttonQueue = document.querySelector('button[data-action]');
+
+
+// додаємо обробники кліка на кнопки, щоб викликати функцію
+buttonWatched.addEventListener('click', handleClick);
+buttonQueue.addEventListener('click', handleClick);
