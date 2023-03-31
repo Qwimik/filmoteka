a3478d481df50ec6bb1094364ca99bdcd4a67ca5
Serhii Halaka <serg.galaka@gmail.com>
2023-04-01 00:05:48 +0300

    added renderModal

--- src/js/render-markup.js
+++ src/js/render-markup.js
@@ -44,6 +44,71 @@ export function renderCardMarkup(data) {
         } else if (first_air_date) {
           cardDate = first_air_date;
         }
+        let cardYear = cardDate.substring(0, 4);
+
+        let cardGenre = genre_ids.map(id => id);
+
+        return `<li class="moviesgallery-item" data-id="${id}">
+            <div class="moviesgallery-wrap">
+            <div class="thumb">
+              <img class="moviesgallery-img" src="${POSTER_BASE_URL}${poster_path}" alt="${title}" width="440" />
+            </div>
+              <div class="moviesgallery-text">
+                <p class="moviesgallery-text-title">${cardTitle}</p>
+                <div class="ganres-wrap">
+                <p class="text">${finalCardGenre}  | ${cardYear}</p>
+                <div class="rating">${rating}</div>
+              </div>
+            </div>
+            </div>
+            </li>`;
+      }
+    )
+    .join(``);
+  cardList.innerHTML = singleCard;
+  return singleCard;
+}
+
+export function renderModalMarkup(data) {
+  const resultArray = data.results;
+  const singleCard = resultArray
+    .map(
+      ({
+        poster_path,
+        title,
+        genre_ids,
+        release_date,
+        vote_average,
+        original_name,
+        first_air_date,
+        id,
+      }) => {
+        let rating = vote_average.toFixed(1);
+
+        let itemGenre = [];
+        let finalCardGenre = [];
+        genre_ids.forEach(function (item) {
+          let genre = genres.map(genre => {
+            if (item === genre.id) {
+              itemGenre.push(genre.name);
+            }
+          });
+          finalCardGenre = itemGenre.join(`, `);
+        });
+
+        let cardTitle = ``;
+        if (title) {
+          cardTitle = title;
+        } else if (original_name) {
+          cardTitle = original_name;
+        }
+
+        let cardDate = ``;
+        if (release_date) {
+          cardDate = release_date;
+        } else if (first_air_date) {
+          cardDate = first_air_date;
+        }
         cardYear = cardDate.substring(0, 4);
 
         let cardGenre = genre_ids.map(id => id);
