2171341056c9742d0569ab87e1cb7d4a3fbed6ee
Oleksii Rozhnenko <qwimik@gmail.com>
2023-04-02 00:43:47 +0300

    fix neon

--- src/js/button.js
+++ src/js/button.js
@@ -1,7 +1,7 @@
 export class Button {
   constructor(id, handlerFunction) {
     this.button = document.querySelector(id);
-    console.log(this.button);
+    // console.log(this.button);
 
     this.button.addEventListener('click', handlerFunction);
   }
