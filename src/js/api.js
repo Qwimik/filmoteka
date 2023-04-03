08955ab4114ef5557b447a8575bd1b66fbd92347
Oleksii Rozhnenko <qwimik@gmail.com>
2023-04-04 00:15:47 +0300

    add trailers

--- src/js/api.js
+++ src/js/api.js
@@ -21,9 +21,21 @@ export const searchMovieTitle = async (title, page) => {
   );
   return res.data;
 };
-export const searchMovieGenre = async (genre) => {
+export const searchMovieGenre = async genre => {
   const res = await axios.get(
     `discover/movie?api_key=${API_KEY}&with_genres=${genre}&language=en-US&include_adult=false&sort_by=revenue.desc`
   );
   return res.data;
+};
+
+//функція яка отримує список відео для фільму з API
+export const getVideos = async movie_id => {
+  const url = `${BASE_URL}/movie/${movie_id}/videos?api_key=${API_KEY}&language=en-US`;
+  try {
+    const response = await axios.get(url);
+    return response.data.results;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
 };
