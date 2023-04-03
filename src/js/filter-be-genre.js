250a57938539f80c1fcec1277fbb935a8a296f2b
INovak90 <112341296+INovak90@users.noreply.github.com>
2023-04-03 15:57:36 +0300

    Add section navigation

---
+++ src/js/filter-be-genre.js
@@ -0,0 +1,18 @@
+import { async } from '@firebase/util';
+import { searchMovieGenre } from './api';
+import { genres } from '/src/data/genres';
+import { renderCardMarkup } from './render-markup';
+const navigation = document.querySelector('.navigation__genre');
+let idGenre = 0;
+navigation.addEventListener('click', onClickFilterByGenre);
+
+async function onClickFilterByGenre(e) {
+  const textContent = e.target.textContent;
+  genres.forEach(element => {
+    if (textContent === element.name) {
+      idGenre = element.id;
+    }
+  });
+  const response = await searchMovieGenre(idGenre);
+  renderCardMarkup(response);
+}
