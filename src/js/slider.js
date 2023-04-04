3875b3daa833b099827c67c034ff93ab2b3cd264
Igor Boiko <igorboiko@gmail.com>
2023-04-04 19:39:43 +0300

    problem with idGenre in onClickFilterByGenre

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,4 +1,25 @@
 import Splide from '@splidejs/splide';
 // import '@splidejs/splide/css';
 
-export const slider = new Splide('.splide').mount();
+export const slider = new Splide('.splide', {
+  type: 'loop',
+  height: '2.5rem',
+  focus: 'center',
+  autoWidth: true,
+  autoHeight: true,
+  arrows: false,
+  perPage: 5,
+  gap: '0.2rem',
+  breakpoints: {
+    1280: {
+      perPage: 8,
+      //   gap: '.2rem',
+      //   height: '2rem',
+    },
+    768: {
+      perPage: 6,
+      //   gap: '0.2rem',
+      //   height: '2rem',
+    },
+  },
+}).mount();
